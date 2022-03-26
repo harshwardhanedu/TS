606ba819f3933f045588466087ae10260fd8025b
harshwardhanedu <noit_web@noitavonne.com>
2022-03-26 11:17:52 +0530

    Day 1

---
+++ app.js
@@ -0,0 +1,10 @@
+var App = /** @class */ (function () {
+    function App() {
+    }
+    App.prototype.test = function () {
+        console.log("Test fun works");
+    };
+    return App;
+}());
+var a1 = new App();
+a1.test();
---
+++ app.ts
@@ -0,0 +1,7 @@
+class App {
+    test(){
+        console.log("Test fun works")
+    }
+}
+let a1=new App();
+a1.test();
