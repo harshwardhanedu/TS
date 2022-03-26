b7406ea06504d890b4a7bd6cd04d1cc2add72fd9
harshwardhanedu <noit_web@noitavonne.com>
2022-03-26 11:53:14 +0530

    day 1.7

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 var data = ['anil0', 'joyo,mario', 10, false];
 data.push(true);
+data.push(799);
 console.log(data);
--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
 let data=['anil0','joyo,mario',10,false];
 data.push(true)
+data.push(799)
 console.log(data);
